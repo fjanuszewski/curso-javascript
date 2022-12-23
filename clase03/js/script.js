22101b32b5116f8d4d396dc65ac79970cef01b7d
Fabian Januszewski <fabian.januszewski@gmail.com>
2022-12-22 21:05:37 -0300

    agrego clase 03

---
+++ clase03/js/script.js
@@ -0,0 +1,44 @@
+console.log("Hola, bienvenido a mi JavaScript");
+let nombre = "";
+
+nombre = prompt("Ingrese el nombre")
+while(nombre ==""){
+    alert("INGRESE UN NOMBRE VALIDO")
+    nombre = prompt("Ingrese el nombre")
+}
+
+do {
+    nombre = prompt("Ingrese el nombre")
+    if (nombre == "") {
+        alert("INGRESE UN NOMBRE VALIDO")
+    }
+} while (nombre =="");
+
+
+// // CASO CON CONDICION DE SALIDA "ESC"
+nombre = prompt("Ingrese el nombre o ESC PARA SALIR")
+while(nombre !="ESC"){
+    nombre = prompt("Ingrese el nombre o ESC PARA SALIR")
+}
+
+do {
+    nombre = prompt("Ingrese el nombre o ESC PARA SALIR")
+} while (nombre !="ESC");
+
+//CASO CON UN FOR
+for (let turno = 1; turno <= 20; turno++) {
+    // En cada repetición solicitamos un nombre.
+    let ingresarNombre = prompt("Ingresar nombre");
+    if(ingresarNombre=="ESC"){
+        break
+    }
+    while(ingresarNombre ==""){
+        ingresarNombre = prompt("Ingrese un nombre valido")
+    }
+    // if(ingresarNombre==""){
+    //     turno=turno-1
+    //     continue
+    // }
+    // Informamos el turno asignado usando el número de repetición (i).
+    alert(" Turno  N° "+turno+" Nombre: "+ingresarNombre);
+}
