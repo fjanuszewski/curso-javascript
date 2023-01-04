419781f01301420881f63dcc84582f2b704d7bcc
Fabian Januszewski <fabian.januszewski@gmail.com>
2023-01-03 21:16:06 -0300

    agrego clase04

--- clase04/js/script.js
+++ clase04/js/script.js
@@ -1,45 +1,57 @@
-console.log("Hola, bienvenido a mi JavaScript");
-let nombre = "";
+//FUNCIONES SIN PARAMETROS
+function saludarSinParametros() {
+    console.log(`¡Hola estudiantes, bienvenidos al curso!`);
+}
+saludarSinParametros() //Asi se invocan las funciones. Invocar = usar
 
-nombre = prompt("Ingrese el nombre")
-while(nombre ==""){
-    alert("INGRESE UN NOMBRE VALIDO")
-    nombre = prompt("Ingrese el nombre")
+//FUNCIONES CON PARAMETROS
+function saludar(curso, segundoParametro) {
+    console.log(`¡Hola estudiantes, bienvenidos al ${curso} este ese mi segundo parametro: ${segundoParametro}!`);
 }
 
-do {
-    nombre = prompt("Ingrese el nombre")
-    if (nombre == "") {
-        alert("INGRESE UN NOMBRE VALIDO")
-    }
-} while (nombre =="");
+let valorUndefined = saludar("SOY EL SEGUNDO PARAMETRO", "CURSO DE JAVASCRIPT") //las funciones sin return devuelven siempre undefined
+console.log(valorUndefined)
 
+//FUNCIONES CON RETURN
+function saludarConReturn() {
+    return "hola chicos de coder" //Se utiliza Return para cortar la funcion y devolver lo que quieran. Ejemplo aca estamos devolviendo un texto, pero podria ser una variable
+}
+// function saludarConReturn(saludo) {
+//     return saludo
+// }
+let saludoConReturn = saludarConReturn()
+console.log(saludoConReturn)
 
-// // CASO CON CONDICION DE SALIDA "ESC"
-nombre = prompt("Ingrese el nombre o ESC PARA SALIR")
-while(nombre !="ESC"){
-    nombre = prompt("Ingrese el nombre o ESC PARA SALIR")
+//SUMAR PARAMETROS
+function sumar(valor1, valor2) {
+    let total = valor1 + valor2 //Total es una variable local. Se puede usar solamente dentro de la funcion
+    return total
+}
+console.log(total)
+let resultado = sumar(21, 12);
+console.log(`el resultado de la suma es: ${resultado}`)
+
+let nombre = "John Doe" // variable global
+
+//VARIABLES LOCALES y GLOBALES
+function saludarConVariablesLocales(texto) {
+    let nombre = "Juan Coder" // variable local
+    console.log(nombre)
 }
 
-do {
-    nombre = prompt("Ingrese el nombre o ESC PARA SALIR")
-} while (nombre !="ESC");
-
-//CASO CON UN FOR
-for (let turno = 1; turno <= 20; turno++) {
-    // En cada repetición solicitamos un nombre.
-    let ingresarNombre = prompt("Ingresar nombre");
-    if(ingresarNombre=="ESC"){
-        break
-    }
-    while(ingresarNombre ==""){
-        ingresarNombre = prompt("Ingrese un nombre valido")
-    }
-    //OTRA MANERA DE EVITAR QUE AVANCE EL INDICE SI EL VALOR ES INVALIDO
-    // if(ingresarNombre==""){
-    //     turno=turno-1
-    //     continue
-    // }
-    // Informamos el turno asignado usando el número de repetición (i).
-    alert(" Turno  N° "+turno+" Nombre: "+ingresarNombre);
+saludarConVariablesLocales()
+//VARIABLES LOCALES y GLOBALES
+function saludarConVariablesGlobales(texto) {
+    nombre = "Juan Coder" // variable local
+    console.log(nombre)
 }
+
+saludarConVariablesGlobales()
+
+let funcionSaludar = () => {
+    nombre = texto // variable local
+    return nombre
+}
+
+//Accede a nombre global
+console.log(funcionSaludar(nombre)) // → “John Doe”
