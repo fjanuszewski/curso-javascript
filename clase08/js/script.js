7ea1b2d032f800db1daa0e69db42bbd658df5156
Fabian Januszewski <fabian.januszewski@gmail.com>
2023-01-12 21:00:51 -0300

    agrego clase 07

---
+++ clase08/js/script.js
@@ -0,0 +1,16 @@
+const items = [
+    { name: 'Pikachu', price: 21 },
+    { name: 'Charmander', price: 37 },
+    { name: 'Pidgey', price: 45 },
+    { name: 'Squirtle', price: 60 }
+  ]
+items.sort((a, b) => {
+    if (a.name > b.name) {
+        return 1;
+    }
+    if (a.name < b.name) {
+        return -1;
+    }
+    // a es igual a b
+    return 0;
+})
